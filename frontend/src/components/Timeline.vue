caac77d4a5892a15b2f4d44a9851a2d701cf6be8
ThomasGuigard <thomasguigard@gmail.com>
2017-06-27 14:32:32 +0200

    Etape 4 et 5 terminées Création du component Timeline et présentation d'une liste de tweets

--- frontend/src/components/Timeline.vue
+++ frontend/src/components/Timeline.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="template">
-
+    <ul>
+        <li v-for="tweet in tweets">{{ tweet.name }} {{ tweet.contenu}}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      tweets: [ { name: 'tweet1', contenu: 'Zbat money' },
+       { name: 'tweet2', contenu: 'J\'en roule un comme Jean-Moulin' },
+       { name: 'tweet3', contenu: 'Brulons les tous jusq\'ua la moelle !' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
+
+</style>
