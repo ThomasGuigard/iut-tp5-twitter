947604a902401102f3096cd72fe8914db6fa090e
ThomasGuigard <thomasguigard@gmail.com>
2017-06-30 14:13:33 +0200

    Amélioration de la présentation des tweets ( etape 10 )

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -5,7 +5,7 @@
     </div>
     <div v-else>
       <ul >
-        <li v-for="tweet in tweets"><tweet :tweet="tweet"/></li>
+        <li class="tweet" v-for="tweet in tweets"><tweet :tweet="tweet"/></li>
       </ul>
     </div>
    </div>
@@ -26,5 +26,17 @@ ul {
 li {
   display: block;
   margin: 0 10px;
+}
+
+li.tweet:first-child {
+ border-top-width: 1px;
+}
+
+li.tweet {
+ border-style: solid;
+ border-width: 0 1px 1px 1px;
+ border-color: gray;
+ width: 500px;
+ margin: auto;
 }
 </style>
--- frontend/src/components/Tweet.vue
+++ frontend/src/components/Tweet.vue
@@ -1,17 +1,54 @@
 <template>
   <div class="tweet">
-    {{ tweet.auteur.prenom }} {{ tweet.auteur.nom }} @{{ tweet.auteur.handle }} - {{ tweet.date }}
-    {{ tweet.contenu }}
+    <div>
+      <span class="handle">{{ tweet.auteur.prenom }} {{ tweet.auteur.nom }}</span> @{{ tweet.auteur.handle }}
+        - {{ tweet.date }}
+    </div>
+
+     <div>
+       {{ tweet.contenu }}
+     </div>
+
+     <div>
+        <ul>
+          <li class="button">
+            <icon name="reply"/>
+          </li>
+          <li class="button">
+            <icon name="retweet"/>
+          </li>
+          <li class="button">
+            <icon name="heart"/>
+          </li>
+          <li class="button">
+            <icon name="envelope"/>
+          </li>
+        </ul>
+      </div>
+
   </div>
 </template>
 
 <script>
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
 export default {
   name: 'tweet',
+  components: {Icon},
   props: ['tweet']
 }
 </script>
 
 <style scoped>
+li.button {
+ display: inline-block;
+}
 
+a {
+ color: #42b983;
+}
+
+span.handle {
+ color: gray;
+}
 </style>
